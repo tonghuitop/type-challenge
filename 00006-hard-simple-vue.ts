646d6155ed76c91faab2ffe992abb72aafa4e2a5
tonghuiwu <tonghuiwu@whale.im>
2022-08-29 10:12:42 +0800

    feat: 添加 hard simple vue

--- 00006-hard-simple-vue.ts
+++ 00006-hard-simple-vue.ts
@@ -40,6 +40,15 @@ SimpleVue({
 
 // ============= Your Code Here =============
 
+type GetComputed<TComputed> = {
+  [key in keyof TComputed]: TComputed[key] extends () => infer Result ? Result : never
+}
+
+type Options<TData, TComputed, TMethods> = {
+  data: (this: void) => TData
+  computed: TComputed & ThisType<TData>
+  methods: TMethods & ThisType<TData & GetComputed<TComputed> & TMethods>
+}
 
 
-declare function SimpleVue(options: any): never
+declare function SimpleVue<TData, TComputed, TMethods>(options: Options<TData, TComputed, TMethods>): unknown
