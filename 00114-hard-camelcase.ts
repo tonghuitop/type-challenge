c52a90ba58c0901ba2b80b1dd7db051f230c2443
tonghuiwu <tonghuiwu@whale.im>
2022-09-16 10:27:14 +0800

    chore: 0916日常刷题

---
+++ 00114-hard-camelcase.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+]
+
+// ============= Your Code Here =============
+type CamelCase<S extends string> = S extends Lowercase<S>
+  ? S extends `${infer L}_${infer H}${infer R}`
+    ? `${L}${Uppercase<H>}${CamelCase<R>}`
+    : S
+  : CamelCase<Lowercase<S>>
